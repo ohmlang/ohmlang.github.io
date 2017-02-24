1d437207800fbf013d861ca25ddba30ca01259a5
Patrick Dubroy <pdubroy@gmail.com>
2017-02-24 16:42:11 +0100

    Update from cdglabs/ohm@9995130

--- dist/test-typings.js
+++ dist/test-typings.js
@@ -9,3 +9,10 @@ var s = g.createSemantics().addOperation('getName', {
 });
 var matchResult = g.match('Ahoy-hoy, Alexander!');
 console.log(s(matchResult).getName());
+var matcher = g.matcher();
+matcher.setInput('foo');
+matcher.replaceInputRange(0, 1, 'g')
+    .replaceInputRange(2, 4, 'ah');
+if (matcher.match('Greeting').succeeded()) {
+    console.log('input:', matcher.getInput());
+}
